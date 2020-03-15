62bf9386352fdb0af777e9d92060ca4d9ca0c6ec
tyasu106 <t.yasu106@gmail.com>
2020-03-15 16:13:36 +0900

    search

---
+++ app/assets/javascripts/users.js
@@ -0,0 +1,68 @@
+$(function() {
+  function addUser(user) {
+    let html = `
+      <div class="chat-group-form__input clearfix">
+        <p class="chat-group-form__input__name">${user.name}</p>
+        <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
+      </div>
+    `;
+    $("#user-search-result").append(html);
+    return html;
+  }
+  function addMembers(user_id) {
+    let html = `
+    <input value="${user_id}" name="group[user_ids][]" type="hidden" id="group_user_ids_${user_id}" />`;
+    $(`#${user_id}`).append(html);
+  }
+
+  function addDeleteUser(name, user_id) {
+    let html = `
+    <div class="chat-group-user clearfix" id="${user_id}">
+    
+      <p class="chat-group-user__name">${name}</p>
+      <div class="user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn" data-user-id="${user_id}" data-user-name="${name}">削除</div>
+    </div>`;
+    $(".js-add-user").append(html);
+  }
+
+
+  $("#user-search-field").on("keyup", function() {
+    let input = $("#user-search-field").val();
+    console.log(input)
+    $.ajax({
+      type: "GET",
+      url: "/users",
+      data: { keyword: input },
+      dataType: "json"
+    })
+      .done(function(users) {
+        $("#user-search-result").empty();
+
+        if (users.length !== 0) {
+        users.forEach(function(user){
+        addUser(user);
+        });
+      } else if (input.length == 0) {
+        return false;
+      } else {
+        addNoUser();
+      }
+      })
+      .fail(function() {
+        alert("ユーザー検索に失敗しました");
+      });
+  });
+  $(function(){
+    $(document).on('click','.user-search-add', function(){
+      var name = $(this).data("user-name");
+      var user_id = $(this).data("user-id");
+      addMembers(name, user_id);
+      addDeleteUser(name, user_id);
+    });
+    $(document).on("click", ".chat-group-user__btn--remove", function() {
+      $(this)
+        .parent()
+        .remove();
+    });
+  })
+});
