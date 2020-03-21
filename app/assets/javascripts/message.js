87882b8b2d3d40f36e42ce936ad78dc68a60bcac
tyasu106 <t.yasu106@gmail.com>
2020-03-21 16:47:47 +0900

    reroad

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -53,16 +53,20 @@ $(function(){
     processData: false,
     contentType: false
   })
+
    .done(function(data){
      var html = buildHTML(data);
      $('.messages').append(html);      
      $('form')[0].reset();
      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
    })
+
    .fail(function(){
     alert('error');
+  })
+  .always(function(){
+    $('.form__submit').removeAttr('disabled')
   });
-
 })
 
  var reloadMessages = function() {
