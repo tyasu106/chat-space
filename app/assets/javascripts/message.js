27d04a1a1308de391680921b1cbaf4dcb26f1e8e
tyasu106 <t.yasu106@gmail.com>
2020-03-21 15:26:06 +0900

    add-yml

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -41,8 +41,33 @@ $(function(){
    };
  }
 
+ $('#new_message').on('submit', function(e){
+  e.preventDefault();
+  var formData = new FormData(this);
+  var url = $(this).attr('action')
+  $.ajax({
+    url: url,
+    type: "POST",
+    data: formData,
+    dataType: 'json',
+    processData: false,
+    contentType: false
+  })
+   .done(function(data){
+     var html = buildHTML(data);
+     $('.messages').append(html);      
+     $('form')[0].reset();
+     $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+   })
+   .fail(function(){
+    alert('error');
+  });
+
+})
+
  var reloadMessages = function() {
-  var last_message_id = $('.message:last').data("message-id");
+  var last_message_id = $('.message__box:last').data("message-id");
+  // console.table(last_message_id)
   $.ajax({
     url: "api/messages",
     type: 'get',
@@ -66,7 +91,9 @@ $(function(){
     alert('error');
   });
  };
+
  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
   setInterval(reloadMessages, 7000);
  }
+
 });
