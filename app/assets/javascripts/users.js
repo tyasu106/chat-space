50fb2c2d73667f84668ab93d7d86f13ab85d3e53
tyasu106 <t.yasu106@gmail.com>
2020-03-23 11:53:30 +0900

    search update

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -11,20 +11,36 @@ $(function() {
   }
 
   function addDeleteUser(name, user_id) {
+    // <input name="group[user_ids][]" type="hidden" value="${user_id}>
     let html = `
     <div class="chat-group-user clearfix" id="${user_id}">
-      <input name="group[user_ids][]" type="hidden" value="${user_id}>
       <p class="chat-group-user__name">${name}</p>
       <div class="user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn" data-user-id="${user_id}" data-user-name="${name}">削除</div>
     </div>`;
+    console.log(html)
     $(".js-add-user").append(html);
   }
 
   function addMembers(user_id) {
     let html = `
     <input value="${user_id}" name="group[user_ids][]" type="hidden" id="group_user_ids_${user_id}" />`;
+    console.log(html)
     $(`#${user_id}`).append(html);
   }
+
+  // function addDeleteUser(name, id) {
+  //   let html = `
+  //   <div class="chat-group-user clearfix" id="${id}">
+  //     <p class="chat-group-user__name">${name}</p>
+  //     <div class="user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn" data-user-id="${id}" data-user-name="${name}">削除</div>
+  //   </div>`;
+  //   $(".js-add-user").append(html);
+  // }
+
+  // function addMembers(userId) {
+  //   let html = `<input value="${userId}" name="group[user_ids][]" type="hidden" id="group_user_ids_${userId}" />`;
+  //   $(`#${userId}`).append(html);
+  // }
 
 
   $("#user-search-field").on("keyup", function() {
@@ -57,8 +73,11 @@ $(function() {
     $(document).on('click','.user-search-add', function(){
       var name = $(this).data("user-name");
       var user_id = $(this).data("user-id");
-      addMembers(name, user_id);
+      addMembers(user_id);
       addDeleteUser(name, user_id);
+      $(this)
+        .parent()
+        .remove();
     });
     $(document).on("click", ".chat-group-user__btn--remove", function() {
       $(this)
